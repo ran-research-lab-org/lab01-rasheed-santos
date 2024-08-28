9ec67d71074a60533d76a59228675d9f8d3bd813
rasheed-santos <rasheed.santos@upr.edu>
2024-08-28 01:44:19 +0000

    cambios lab01

---
+++ lab01.cpp
@@ -0,0 +1,19 @@
+//Rasheed Santos(rasheed-santos)
+//CCOM 3033 - LAB01
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello Solar System!" << std::endl;
+  std::cout << "Hello Galaxy!" << std::endl;
+  std::cout << "Hello Universe!" << std::endl;
+  std::cout << "Hello ... ?" << std::endl;
+  std::cout << "This is a 1!" << std::endl;
+  std::cout << "This is a 2!" << std::endl;
+  std::cout << "This is a 3!" << std::endl;
+  std::cout << "This is a 4!" << std::endl;
+  std::cout << "This is a 5!" << std::endl;
+
+  return 0;
+}
