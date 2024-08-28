41035df6dffa625de98b03b21051bfe3949b94fe
rasheed-santos <rasheed.santos@upr.edu>
2024-08-28 00:56:12 +0000

    confirmar

--- main.cpp
+++ main.cpp
@@ -3,5 +3,21 @@
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+  std::cout << "Hello World!" << std::endl;
+  std::cout << "Hello Solar System!" << std::endl;
+  std::cout << "Hello Galaxy!" << std::endl;
+  std::cout << "Hello Universe!" << std::endl;
+  std::cout << "Hello ... ?" << std::endl;
+  std::cout << "This is a 1!" << std::endl;
+  std::cout << "This is a 2!" << std::endl;
+  std::cout << "This is a 2!" << std::endl;
+  std::cout << "This is a 2!" << std::endl;
+  std::cout << "This is a 2!" << std::endl;
+
+  return 0;
 }
+
+
+
+
+
